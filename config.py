246855c0342d2b5400225f86b37bdca9dc4439d5
ismailhashinkc-jpg <ismailhashinkc@gmail.com>
2025-10-23 20:58:43 -0500

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key-here'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://user:password@localhost/dbname'  # Will be set by Render
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://user:password@localhost/dbname'  # Render will provide this
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     WTF_CSRF_ENABLED = True
